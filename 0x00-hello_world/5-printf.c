6a594b41ea2139cc682437cf69141fc78515263a
Meramore <merongizaw92@yahoo.com>
2022-06-06 15:06:24 -0700

    do

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>0
+#include<stdio.h>
 /**
- * *main - print string
- *
- * Return: Always 0
- */
+*main - print string
+*
+* Return: Always 0
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
